3b9aa2a4a9774ee47ead8a0a367b1fe808b39a7f
Yaroslav Yaroslavcev <y.yaroslavtsev@innopolis.ru>
2022-06-14 12:35:42 +0700

    fixed weights saving and updated .gitignore

--- MLL/Saving/NeuronWeightsSaver.cs
+++ MLL/Saving/NeuronWeightsSaver.cs
@@ -5,13 +5,13 @@ namespace MLL.Saving;
 
 public static class NeuronWeightsSaver
 {
-    private const string DirPath = "../../../Data";
+    private const string DirPath = "../../../Data/";
     private const string Filename = "/hiddennetsig1.json";
     
     public static void Save(Net net)
     {
         Directory.CreateDirectory(DirPath);
-        var filename = Path.Combine(DirPath, Filename);
+        var filename = DirPath + Filename;
 
         var json = JsonConvert.SerializeObject(net);
         File.WriteAllText(filename, json);
@@ -19,7 +19,7 @@ public static class NeuronWeightsSaver
 
     public static Net Load()
     {
-        var filename = Path.Combine(DirPath, Filename);
+        var filename = DirPath + Filename;
         var json = File.ReadAllText(filename);
         return JsonConvert.DeserializeObject<Net>(json)!;
     }
