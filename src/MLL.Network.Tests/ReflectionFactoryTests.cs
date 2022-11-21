90d50e0f5b58a6e643af10faa1d96e32c7e0f33f
yaroslav <alienmonsterrus@gmail.com>
2022-11-21 10:59:03 +0700

    Added ReflectionMessageHandlerFactory

---
+++ src/MLL.Network.Tests/ReflectionFactoryTests.cs
@@ -0,0 +1,67 @@
+﻿using MLL.Network.Factories;
+using MLL.Network.Message.Protocol;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace MLL.Network.Tests;
+
+[TestFixture]
+public class ReflectionFactoryTests
+{
+    public class EmptyCtorModel
+    {
+    }
+
+    public class SenderCtorModel
+    {
+        public IMessageSender Sender { get; }
+
+        public SenderCtorModel(IMessageSender sender)
+        {
+            Sender = sender;
+        }
+    }
+
+    public class PreferSenderCtorModel
+    {
+        public IMessageSender? Sender { get; }
+
+        public PreferSenderCtorModel(IMessageSender sender)
+        {
+            Sender = sender;
+        }
+
+        public PreferSenderCtorModel() { }
+    }
+
+    [Test]
+    public void EmptyCtor()
+    {
+        var reflectionFactory = new ReflectionMessageHandlerFactory<EmptyCtorModel>();
+        var handler = reflectionFactory.CreateMessageHandler(new MessageHandlerFactoryContext());
+        Assert.IsInstanceOf<EmptyCtorModel>(handler);
+    }
+
+    [Test]
+    public void SenderCtor()
+    {
+        var senderMock = Mock.Of<IMessageSender>();
+        var reflectionFactory = new ReflectionMessageHandlerFactory<SenderCtorModel>();
+        var handler = reflectionFactory.CreateMessageHandler(new(senderMock, Guid.NewGuid()));
+
+        Assert.IsInstanceOf<SenderCtorModel>(handler);
+        Assert.IsNotNull(((SenderCtorModel) handler).Sender);
+    }
+
+    [Test]
+    public void PreferSenderCtor()
+    {
+        var senderMock = Mock.Of<IMessageSender>();
+        var reflectionFactory = new ReflectionMessageHandlerFactory<PreferSenderCtorModel>();
+        var handler = reflectionFactory.CreateMessageHandler(new(senderMock, Guid.NewGuid()));
+
+        Assert.IsInstanceOf<PreferSenderCtorModel>(handler);
+        Assert.IsNotNull(((PreferSenderCtorModel)handler).Sender);
+    }
+}
--- src/MLL.Network/Factories/IMessageHandlerFactory.cs
+++ src/MLL.Network/Factories/IMessageHandlerFactory.cs
@@ -1,14 +1,87 @@
-﻿using MLL.Network.Message.Handlers;
-using MLL.Network.Message.Protocol;
+﻿using MLL.Network.Message.Protocol;
 using System;
-using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace MLL.Network.Factories;
 
 public interface IMessageHandlerFactory
 {
-    IMultiMessageHandler CreateMessageHandler(MessageHandlerFactoryContext context);
-    IEnumerable<Type> GetSendedTypes();
+    object CreateMessageHandler(MessageHandlerFactoryContext context);
+    Type MessageHandlerType { get; }
+}
+
+public class ReflectionMessageHandlerFactory<THandler> : IMessageHandlerFactory
+    where THandler : class
+{
+    public Type MessageHandlerType => typeof(THandler);
+
+    private readonly Func<MessageHandlerFactoryContext, THandler> _handlerFactory;
+
+    public ReflectionMessageHandlerFactory()
+    {
+        var handlerType = typeof(THandler);
+
+        var ctorInfos = handlerType.GetConstructors()
+            .Select(c => new { Ctor = c, Params = c.GetParameters() })
+            .Where(x => x.Params.Length < 2)
+            .OrderByDescending(x => x.Params.Length);
+
+        foreach (var ctorInfo in ctorInfos)
+        {
+            var handlerFactory = Generate(ctorInfo.Ctor);
+
+            if (handlerFactory != null)
+            {
+                _handlerFactory = handlerFactory;
+                return;
+            }
+        }
+
+        throw new InvalidOperationException(
+            $"Constructor accepting {nameof(IMessageSender)} or empty constructor not found.");
+    }
+
+    public object CreateMessageHandler(MessageHandlerFactoryContext context)
+    {
+        return _handlerFactory.Invoke(context)!;
+    }
+    
+    private Func<MessageHandlerFactoryContext, THandler>? Generate(ConstructorInfo ctor)
+    {
+        var parameters = ctor.GetParameters();
+        var param = parameters.FirstOrDefault();
+
+        var isAcceptingSender = param?.ParameterType == typeof(IMessageSender);
+
+        if (parameters.Length != 0 && !isAcceptingSender)
+        {
+            return null;
+        }
+
+        var inputTypes = new[] { typeof(MessageHandlerFactoryContext) };
+        var method = new DynamicMethod(string.Empty, typeof(THandler), inputTypes);
+
+        var il = method.GetILGenerator();
+
+        if (isAcceptingSender)
+        {
+            var contextType = typeof(MessageHandlerFactoryContext);
+            var senderPropertyName = nameof(MessageHandlerFactoryContext.MessageSender);
+            var senderProperty = contextType.GetProperty(senderPropertyName);
+
+            il.Emit(OpCodes.Ldarga_S, (byte)0);
+            il.Emit(OpCodes.Callvirt, senderProperty.GetGetMethod());
+        }
+
+        il.Emit(OpCodes.Newobj, ctor);
+        il.Emit(OpCodes.Ret);
+
+        var func = method.CreateDelegate(typeof(Func<MessageHandlerFactoryContext, THandler>));
+        return (Func<MessageHandlerFactoryContext, THandler>)func;
+    }
+
 }
 
 public readonly struct MessageHandlerFactoryContext
